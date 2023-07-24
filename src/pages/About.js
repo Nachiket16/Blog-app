b38b95efdfc7d98a82efb64612537fd34f457eda
Nachiket16 <nachiketkhule.nk@gmail.com>
2023-07-24 20:51:50 +0530

    wip

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import Base from "../component/Base";
+
+const About = ()=>{
+    return 
+    <Base>
+        <h1>This is about page</h1>
+        <p>we are building blog website</p>
+    </Base>
+};
+export default About;
