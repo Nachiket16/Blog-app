09fcdece1d963cd32b598043f582e33132e394f2
Nachiket Khule <nachiketkhule.nk@gmail.com>
2023-09-18 20:07:58 +0530

    updated index.js

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,30 +1,30 @@
 //isLoggedIn
-export const isLoggedIn=()=>{
+export const isLoggedIn = () => {
     let data = localStorage.getItem("data")
-    if(data==null){
+    if (data == null) {
         return false;
     }
-    else{
+    else {
         return true;
     }
 }
 //doLogin -> set to localStorage
-export const doLogin=(data,next)=>{
-    localStorage.setItem("data",JSON.stringify(data));
+export const doLogin = (data, next) => {
+    localStorage.setItem("data", JSON.stringify(data));
     next();
 }
 
 //doLogout-> remove from localStorage
-export const doLogout=(next)=>{
+export const doLogout = (next) => {
     localStorage.removeItem("data")
     next();
 }
 
 //get currentUser
-export const getCurrentUserDetail=()=>{
-    if(isLoggedIn){
+export const getCurrentUserDetail = () => {
+    if (isLoggedIn) {
         return JSON.parse(localStorage.getItem("data"));
-    }else{
+    } else {
         return undefined;
     }
 }
